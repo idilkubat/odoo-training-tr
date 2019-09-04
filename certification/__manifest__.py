7b669c0ed608a6a22bd2a058b59b719211fa655a
idilkubat <idil.kubat44@hotmail.com>
2019-09-04 20:57:10 +0300

    Certification module

--- certification/__manifest__.py
+++ certification/__manifest__.py
@@ -9,11 +9,14 @@
     'version': '12.0.1.0.0',
     'license': 'AGPL-3',
     'depends': ['base'],
-    'data': ['security/ir.model.access.csv',
-             'security/certification_security.xml',
+    'data': ['security/certification_security.xml',
+             'security/ir.model.access.csv',
              'views/certification_view.xml',
              'views/res_partner_view.xml',
-             'views/standard_view.xml'
+             'views/standard_view.xml',
+             'reports/certification_report.xml',
+             'reports/certification_template_pdf.xml',
+             'reports/report_certification_pdf.xml'
              ],
 
     'demo': ['data/certification_data.xml'],
